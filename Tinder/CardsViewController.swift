2ed4eebf880b7922c8860455b7cbf6ea71eb5d81
Israel Andrade <israel_andrade@Israels-MacBook-Pro.local>
2018-03-29 12:54:55 -0700

    Add animation to the image view

--- Tinder/CardsViewController.swift
+++ Tinder/CardsViewController.swift
@@ -30,7 +30,7 @@ class CardsViewController: UIViewController {
         print("translation \(translation)")
         let velocity = sender.velocity(in: view)
         
-
+//If the x translation is greater than 50, animate it off screen to the right. If the x translation is less than 50, animate it off screen to the left
         if sender.state == .began {
             cardInitialCenter = profileImage.center
             
@@ -61,14 +61,33 @@ class CardsViewController: UIViewController {
                 profileImage.transform = CGAffineTransform(rotationAngle: CGFloat(negative * M_PI / 180))
 
             }
-            profileImage.center = CGPoint(x: cardInitialCenter.x + translation.x, y: cardInitialCenter.y + translation.y)
+//            profileImage.center = CGPoint(x: cardInitialCenter.x + translation.x, y: cardInitialCenter.y + translation.y)
 
             
         } else if sender.state == .ended {
-            self.rotaion = 0
-             profileImage.center = cardInitialCenter
-            profileImage.transform = CGAffineTransform(rotationAngle: CGFloat(0 * M_PI / 180))
+            if translation.x > 50 {
+                //right
+                UIView.animate(withDuration: 0.3) {
+                    self.profileImage.center =
+                        CGPoint(x: self.cardInitialCenter.x + 500 ,y: self.cardInitialCenter.y)
+                }
+            }else if(translation.x < 50){
+                //left
+                UIView.animate(withDuration: 0.3) {
+                    //self.profileImage.center
+                    self.profileImage.center =
+                        CGPoint(x: self.cardInitialCenter.x - 500 ,y: self.cardInitialCenter.y)
+                }
+                
+            }
+            else {
+                self.rotaion = 0
+                profileImage.center = cardInitialCenter
+                profileImage.transform = CGAffineTransform(rotationAngle: CGFloat(0 * M_PI / 180))
+            }
+            profileImage.transform = CGAffineTransform.identity
 
+            //view.transform = CGAffineTransform.identity
         }
         
     }
